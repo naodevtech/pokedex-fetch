67fa90565b83a0dfeec9170bb2fe2c7b90318d6b
naimtahouri <naimtahouri@gmail.com>
2020-04-29 20:04:47 +0200

    pokedex spawned on my github

---
+++ js/index.js
@@ -0,0 +1,34 @@
+console.log('hello')
+const container = document.querySelectorAll('.container')
+const boxPokemon = document.querySelectorAll('.box')
+const containerbis = document.getElementsByClassName('container')
+let array = []
+function fetching(){
+    for(let i = 1; i < 151; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon-form/${i}/?limit=151`
+        fetch(url)
+        .then( data => data.json())
+        .then( pokemon => {
+            // console.log(pokemon)
+            const box = document.createElement('div')
+            box.setAttribute('class', 'box')
+            let title = document.createElement('h1')
+            title.innerHTML = pokemon.name
+            box.appendChild(title)
+            let img = document.createElement('img')
+            img.setAttribute('src', pokemon.sprites.front_default)
+            box.appendChild(img)
+            container[0].appendChild(box)
+            // console.log(box)
+            
+        })
+    }
+}
+fetching()
+
+let histories = JSON.parse(localStorage.getItem('pokemons')) || [];
+console.log(histories)
+
+boxPokemon.forEach((box) => box.addEventListener('click', (e) =>{
+    console.log(e)
+}))
